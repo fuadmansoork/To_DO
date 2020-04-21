8d0b84c3ade02124bd742abcf48eceb1f055de7b
unknown <fuadmansoork@gmail.com>
2020-04-21 15:21:27 +0530

    initial commit

---
+++ app/routes.py
@@ -0,0 +1,31 @@
+from app import app, forms
+from flask import render_template, request
+from app import db
+from app.models import Feedback
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/feedback', methods=['GET', 'POST'])
+def feedback():
+    form = forms.FeedbackForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        text = form.text.data
+
+        feedback = {'name':name, 'text':text}
+
+        f = Feedback(name=name, text=text)
+        db.session.add(f)                   
+        db.session.commit()               
+
+        return render_template('thanks.html', feedback=feedback)
+
+    return render_template('feedback.html', form=form)
+
+@app.route('/show')
+def  show_feedbacks():
+	feedbacks = Feedback.query.all()
+	return render_template('show.html', feedbacks=feedbacks)
